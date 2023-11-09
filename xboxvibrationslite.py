9fcf4a676c71845369bb871ff57b30bbbee52c18
LukysGaming <49752472+LukysGaming@users.noreply.github.com>
2023-11-09 18:01:50 +0100

    add pressed keys

--- xboxvibrationslite.py
+++ xboxvibrationslite.py
@@ -12,6 +12,23 @@ class State(ctypes.Structure):
 
 xinput = ctypes.windll.xinput1_4
 
+BUTTON_NAMES = {
+    0x1000: "A",
+    0x2000: "B",
+    0x4000: "X",
+    0x8000: "Y",
+    # Add more button mappings as needed
+}
+
+# Replace the ASCII art with your own representation
+BUTTON_ART = {
+    "A": [" .----------------. ", "| .--------------. |", "| |      __      | |", "| |     /  \\     | |", "| |    / /\\ \\    | |", "| |   / ____ \\   | |", "| | _/ /    \\ \\_ | |", "| ||____|  |____|| |", "| |              | |", "| '--------------' |", " '----------------' "],
+    "B": [" .----------------. ", "| .--------------. |", "| |   ______     | |", "| |  |_   _ \\    | |", "| |    | |_) |   | |", "| |    |  __'.   | |", "| |   _| |__) |  | |", "| |  |_______/   | |", "| |              | |", "| '--------------' |", " '----------------' "],
+    "X": [" .----------------. ", "| .--------------. |", "| |  ____  ____  | |", "| | |_  _||_  _| | |", "| |   \\ \\  / /   | |", "| |    > `' <    | |", "| |  _/ /'`\\ \\_  | |", "| | |____||____| | |", "| |              | |", "| '--------------' |", " '----------------' "],
+    "Y": [" .----------------. ", "| .--------------. |", "| |  ____  ____  | |", "| | |_  _||_  _| | |", "| |   \\ \\  / /   | |", "| |    \\ \\/ /    | |", "| |    _|  |_    | |", "| |   |______|   | |", "| |              | |", "| '--------------' |", " '----------------' "],
+    # Add more button ASCII art as needed
+}
+
 def set_vibration(index, left_speed, right_speed):
     vibration = Vibration(left_speed, right_speed)
     xinput.XInputSetState(index, ctypes.byref(vibration))
@@ -19,13 +36,24 @@ def set_vibration(index, left_speed, right_speed):
 def is_button_pressed(state, button):
     return (state.gamepad.buttons & button) != 0
 
+def get_pressed_button(state):
+    for button, name in BUTTON_NAMES.items():
+        if is_button_pressed(state, button):
+            return name
+    return None
+
+def print_button_art(button_name):
+    art_lines = BUTTON_ART.get(button_name, ["", "", ""])
+    for line in art_lines:
+        print(line)
+
 def print_vibration_intensity(left_speed, right_speed):
     left_percentage = (left_speed / 65535) * 100
     right_percentage = (right_speed / 65535) * 100
 
     print("Vibration Intensity:")
-    print(f"   Left:  {'█' * (left_speed // 3277)}  {left_percentage:.2f}%  Value: {left_speed}")
-    print(f"   Right: {'█' * (right_speed // 3277)}  {right_percentage:.2f}%  Value: {right_speed}")
+    print(f"   Left:  {'█' * int(left_speed / 3277)}  {left_percentage:.2f}%  Value: {left_speed}")
+    print(f"   Right: {'█' * int(right_speed / 3277)}  {right_percentage:.2f}%  Value: {right_speed}")
 
 def main():
     index, enabled = 0, False
@@ -36,13 +64,17 @@ def main():
             state = State()
             xinput.XInputGetState(index, ctypes.byref(state))
 
-            if is_button_pressed(state, 0xFFFF):
+            pressed_button = get_pressed_button(state)
+
+            if pressed_button is not None:
                 if not button_pressed:
                     button_pressed = True
                     enabled = not enabled
                     left_speed, right_speed = (65535, 65535) if enabled else (0, 0)
                     set_vibration(index, left_speed, right_speed)
                     print_vibration_intensity(left_speed, right_speed)
+                    print(f"Button {pressed_button} pressed")
+                    print_button_art(pressed_button)
             else:
                 button_pressed = False
 
